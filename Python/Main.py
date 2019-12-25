bdf448c6f47c86b200acd3a285cd4313b5137550
leomatamoros <leomatamoros7@yahoo.es>
2019-12-25 20:00:52 +0100

    Metodo constructor en Python -Clases:Account y Car

--- Python/Main.py
+++ Python/Main.py
@@ -1,12 +1,15 @@
-from Car import car
+from car import Car
+from account import Account
+
 if __name__=="__main__":
     print("Hola Mundo")
-    car = Car()
-    car.license = "123-QWA"
-    car.driver = "Juan"
+    car = Car("AMS234", Account("Andres Herrera", "ANDA876"))
+    #car.license = "123-QWA"
+    #car.driver = "Juan"
     print(vars(car))
+    print(vars(car.driver))
 
-    car2 = Car()
-    car2.license = "456-you"
-    car2.driver = "Alex"
+    car2 = Car("AMS234+1", Account("Andres Herrera+1", "ANDA876+1"))
+    #car2.license = "456-you"
+    #car2.driver = "Alex"
     print(vars(car2))
--- Python/car.py
+++ Python/car.py
@@ -1,5 +1,8 @@
 class Car:
-id        = int
-license   = str
-driver    = str
-passanger = int
+    id        = int
+    license   = str
+    driver    = str
+    passanger = int
+    def __init__(self, license, driver):
+        self.license = license
+        self.driver = driver
